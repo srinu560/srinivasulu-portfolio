601fd193bb1e95d7a02b2a281b53690d70cd8e60
Srinivasulu Reddy Sandireddy <149289862+srinu560@users.noreply.github.com>
2025-10-18 15:38:30 +0530

    Update vite.config.ts
    Updating base folder name

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/my-portfolio-site/',
+  base: '/srinivasulu-portfolio/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
